dc25cb85b3d7b5702340191447e6aca7ac2d5c89
Gera <gr.misterg@gmail.com>
2023-04-05 15:23:08 -0600

    se agrego algo del script

---
+++ scripts.js
@@ -0,0 +1,42 @@
+const rines=[
+  {
+    "codigo": 123,
+    "titulo": "H",
+    "precio": 99,
+    "cantidad": 0
+  },
+  {
+    "codigo": 333,
+    "titulo": "l",
+    "precio": 50,
+    "cantidad": 0
+  }
+];
+
+const carrito=[]
+
+var botones=document.getElementsByClassName("agregar");
+
+for (var i = 0; i < botones.length; i++){
+  botones[i].addEventListener("click", function(){
+
+  })
+}
+
+function agregarCarrito(boton){
+  var precio=boton.querySelector();
+}
+
+//Agregamos que una función suceda cuando se de el evento click
+// function agregar(codigo){
+//   for (var i = 0; i < rines.length; i++){
+//     var rin = rines[i]
+//     if (rin["codigo"]==codigo) {
+//       rin["cantidad"]++;     
+//     }
+//   }
+//   console.log(rines);
+// };
+
+//TODO: Hacer esto mismo pero usando localstoge. Es decir, crear el arreglo al ejecutar el boton, como en el script de la ultima clase
+
