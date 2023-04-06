0a9b7186df05854858f1137f97e2d5e0315e59b7
Gera <gr.misterg@gmail.com>
2023-04-05 20:09:36 -0600

    Se terminó la primera versión del script para agregar al carrito

--- scripts.js
+++ scripts.js
@@ -1,80 +1,33 @@
-const rines=[
-  {
-    "codigo": 123,
-    "titulo": "H",
-    "precio": 99,
-    "cantidad": 0
-  },
-  {
-    "codigo": "333",
-    "titulo": "l",
-    "precio": 50,
-    "cantidad": 0
-  }
-];
+var carrito=[];
 
-// const carrito=[]
+var botones=document.querySelectorAll(".agregar");
 
-var botones=document.getElementsByClassName("agregar");
-
-for (var i = 0; i <= botones.length; i++){
-  botones[i].addEventListener("click", function(){
-
-    cache=localStorage.getItem("carrito");
-
-    if(cache==NULL){
-      carrito=[]
-    }else{
-      carrito=JSON.parse(cache);
-    }
-
-    const boton = botones[i];
-    const contenedor = boton.closest(".contenedor");
+botones.forEach(boton =>{
+  boton.addEventListener("click", () => {
     
-    const codigo=contenedor.getElementsByClassName("codigo").value;
+    var contenedor = boton.closest(".contenedor");
     
-    const titulo=contenedor.getElementsByClassName("titulo").value;
-    
-    const precio=contenedor.getElementsByClassName("precio").value;
-    
-    const cantidad=0;
+    var codigo=contenedor.querySelector(".codigo").textContent;
+  
+    var modelo=contenedor.querySelector(".modelo").textContent;
 
+    var tipo=contenedor.querySelector(".tipo").textContent;
+    
+    var precio=contenedor.querySelector(".precio").textContent;
+    
     //Creamos nuestro elemento
-    const rin={
-      "codigo": codigo,
-      "titulo": titulo,
-      "precio": precio,
-      "cantidad": cantidad
-    }
+    var rin={
+      codigo: codigo,
+      modelo: modelo,
+      tipo: tipo,
+      precio: precio
+    };
 
-    for(var j=0; j<= carrito.length; j++){
-      if(carrito[j].codigo==rin.codigo){
-        rin.cantidad++;
-      }else{
-        carrito.push(rin);
-      }
-      console.log(carrito);
-    }
-    localStorage.setItem("carrito", JSON.stringify(carrito))
-    
+    carrito.push(rin);
+
+    console.log(Array.from(carrito));
     //TODO: crear las  cajas en el carrito
-  })
-}
+  });
+});
 
-// function agregarCarrito(boton){
-//   var precio=boton.querySelector();
-// }
-
-//Agregamos que una función suceda cuando se de el evento click
-// function agregar(codigo){
-//   for (var i = 0; i < rines.length; i++){
-//     var rin = rines[i]
-//     if (rin["codigo"]==codigo) {
-//       rin["cantidad"]++;     
-//     }
-//   }
-//   console.log(rines);
-// };
-
-//TODO: Hacer esto mismo pero usando localstoge. Es decir, crear el arreglo al ejecutar el boton, como en el script de la ultima clase
 
